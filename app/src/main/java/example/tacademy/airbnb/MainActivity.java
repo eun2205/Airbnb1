4a342566cda6a6986758ad64f17031e205a851c6
Eun Ji <kwakeunjea@naver.com>
2016-08-18 02:03:58 +0900

    hamburger

--- app/src/main/java/example/tacademy/airbnb/MainActivity.java
+++ app/src/main/java/example/tacademy/airbnb/MainActivity.java
@@ -2,23 +2,112 @@ package example.tacademy.airbnb;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+    DrawerLayout drawer;
+    NavigationView naviView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        FloatingActionButton fab = (FloatingActionButton)findViewById(R.id.fab_click);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, SearchActivity.class);
-                startActivity(intent);
+//        FloatingActionButton fab = (FloatingActionButton)findViewById(R.id.fab_click);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(MainActivity.this, SearchActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+        setSupportActionBar((Toolbar)findViewById(R.id.toolbar));
+
+        drawer = (DrawerLayout)findViewById(R.id.drawer);
+        naviView = (NavigationView)findViewById(R.id.navi_menu);
+        naviView.setNavigationItemSelectedListener(this);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.sample_hamburger);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_search : {
+                startActivity(new Intent(MainActivity.this, SearchActivity.class ));
+                break;
             }
-        });
+            case R.id.menu_home : {
+
+                break;
+            }
+            case R.id.menu_message : {
+
+                break;
+            }
+            case R.id.menu_travel : {
+                break;
+            }
+            case R.id.menu_wishlist : {
+                break;
+            }
+            case R.id.menu_profile : {
+                break;
+            }
+            case R.id.menu_hosting : {
+                break;
+            }
+            case R.id.menu_setting : {
+                break;
+            }
+            case R.id.menu_help : {
+                break;
+            }
+            case R.id.menu_invitate : {
+                break;
+            }
+            case R.id.menu_feedback : {
+                break;
+            }
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            if (drawer.isDrawerOpen(GravityCompat.START)) {
+                drawer.closeDrawer(GravityCompat.START);
+            } else {
+                drawer.openDrawer(GravityCompat.START);
+            }
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+//    private void emptyBackStack() {
+//        getSupportFragmentManager().popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE);
+//    }
+//
+//    private void replaceFragment(Fragment f) {
+//        getSupportFragmentManager().beginTransaction()
+//                .replace(R.id.container, f)
+//                .addToBackStack(null)
+//                .commit();
+//    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
