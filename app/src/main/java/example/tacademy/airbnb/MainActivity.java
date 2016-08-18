30b010bb4a1afd22305f790cc5c0980546f73d86
Eun Ji <kwakeunjea@naver.com>
2016-08-18 16:59:39 +0900

    spinner fail

--- app/src/main/java/example/tacademy/airbnb/MainActivity.java
+++ app/src/main/java/example/tacademy/airbnb/MainActivity.java
@@ -1,15 +1,22 @@
 package example.tacademy.airbnb;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     DrawerLayout drawer;
     NavigationView naviView;
@@ -26,58 +33,97 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 //                startActivity(intent);
 //            }
 //        });
-        setSupportActionBar((Toolbar)findViewById(R.id.toolbar));
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
 
-        drawer = (DrawerLayout)findViewById(R.id.drawer);
-        naviView = (NavigationView)findViewById(R.id.navi_menu);
+        drawer = (DrawerLayout) findViewById(R.id.drawer);
+        naviView = (NavigationView) findViewById(R.id.navi_menu);
         naviView.setNavigationItemSelectedListener(this);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.sample_hamburger);
     }
 
+    AlertDialog dialog;
+    private ArrayAdapter<String> mAdapter = null;
+
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case R.id.menu_search : {
-                startActivity(new Intent(MainActivity.this, SearchActivity.class ));
+            case R.id.menu_search: {
+                startActivity(new Intent(MainActivity.this, SearchActivity.class));
                 break;
             }
-            case R.id.menu_home : {
+            case R.id.menu_home: {
 
                 break;
             }
-            case R.id.menu_message : {
+            case R.id.menu_message: {
 
                 break;
             }
-            case R.id.menu_travel : {
+            case R.id.menu_travel: {
                 break;
             }
-            case R.id.menu_wishlist : {
+            case R.id.menu_wishlist: {
                 break;
             }
-            case R.id.menu_profile : {
+            case R.id.menu_profile: {
                 break;
             }
-            case R.id.menu_hosting : {
+            case R.id.menu_hosting: {
                 break;
             }
-            case R.id.menu_setting : {
+            case R.id.menu_setting: {
                 break;
             }
-            case R.id.menu_help : {
+            case R.id.menu_help: {
                 break;
             }
-            case R.id.menu_invitate : {
+            case R.id.menu_invitate: {
                 break;
             }
-            case R.id.menu_feedback : {
+            case R.id.menu_feedback: {
+                Spinner spinner = (Spinner) this.findViewById(R.id.spinner_feedback);
+                mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, (String[])getResources().getStringArray(R.array.spinner));
+                mAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                // sp.setOnItemSelectedListener(this);
+                LayoutInflater inflater = getLayoutInflater();
+                View dialoglayout = inflater.inflate(R.layout.dialog_feedback, null);
+                final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+                //              final EditText explain = new EditText(this);
+                builder.setTitle("에어비앤비에 피드백 보내기");
+                builder.setMessage("에어비앤비 앱에 대한 피드백을 이메일로 보내주세요.");
+                builder.setPositiveButton("피드백 보내기", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Intent intent = new Intent(Intent.ACTION_VIEW,
+                                Uri.parse("http://www.gmail.com"));
+                        startActivity(intent);
+                    }
+                });
+
+                builder.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        builder.setCancelable(false);
+                    }
+                });
+                builder.setCancelable(false);
+
+                //               LinearLayout layout = new LinearLayout(getApplicationContext());
+                //             layout.addView(explain);
+                builder.setView(dialoglayout);
+                spinner.setAdapter(mAdapter);
+                dialog = builder.create();
+                dialog.show();
+
                 break;
             }
         }
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == android.R.id.home) {
@@ -90,17 +136,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         }
         return super.onOptionsItemSelected(item);
     }
-
-//    private void emptyBackStack() {
-//        getSupportFragmentManager().popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE);
-//    }
-//
-//    private void replaceFragment(Fragment f) {
-//        getSupportFragmentManager().beginTransaction()
-//                .replace(R.id.container, f)
-//                .addToBackStack(null)
-//                .commit();
-//    }
 
     @Override
     public void onBackPressed() {
